f6d525ee45cdeb2a08ac0919ee4e91621c4bb7e8
Andy Hsu <i@nn.ci>
2026-01-03 20:44:42 +0800

    feat: initial commit

---
+++ DisplayCut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DisplayCut
+//
+//  Created by xhofe on 2026/1/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DisplayCut/DisplayCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayCutApp.swift
+//  DisplayCut
+//
+//  Created by xhofe on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct DisplayCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
